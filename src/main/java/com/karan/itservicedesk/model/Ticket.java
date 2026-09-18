4739eade752570e7b27c73a89e63c98309515580
Karan dodis <karandodis503@gmail.com>
2026-09-18 22:37:28 +0530

    Add automatic ticket creation timestamp

--- src/main/java/com/karan/itservicedesk/model/Ticket.java
+++ src/main/java/com/karan/itservicedesk/model/Ticket.java
@@ -1,11 +1,14 @@
 package com.karan.itservicedesk.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
@@ -29,6 +32,23 @@ public class Ticket {
 
 	@Enumerated(EnumType.STRING)
 	private Status status;
+	
+	
+	private LocalDateTime createdAt;
+	
+	@PrePersist
+	public void prePersist() {
+		
+		createdAt = LocalDateTime.now();
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
 
 	public Long getId() {
 		return id;
